97885157419a0bf3a19b14bc437073aa5206dc09
themindfulcreator <themindfulcreator@gmail.com>
2021-05-10 23:52:48 +0800

    Initial commit: This piece of code is a Mortgage Calculator written in JAVA (programming language).

---
+++ src/com/themindfulcreator/Main.java
@@ -0,0 +1,34 @@
+package com.themindfulcreator;
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// This piece of code is a Mortgage Calculator written in JAVA (programming language).
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Principal: ");
+        int principal = scanner.nextInt();
+
+        System.out.print("Annual Interest Rate: ");
+        float annualInterest = scanner.nextFloat();
+        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+
+        System.out.print("Period (Years): ");
+        byte years = scanner.nextByte();
+        int numberOfPayments = years * MONTHS_IN_YEAR;
+
+        double mortgage = principal
+                * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments, numberOfPayments) -1);
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format();
+        System.out.println("Mortgage: " + mortgageFormatted);
+    }
+
+}
